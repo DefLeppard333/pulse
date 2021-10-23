fcfe80e0c595fc4e8fbbe1a0d8dc5e542b82e664
Igor <defleppdard333@gmail.com>
2021-10-23 21:42:25 +0300

    refactor: change slider to tity slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,14 @@
+const slider = tns({
+   container: '.carousel__inner',
+   items: 1,
+   slideBy: 'page',
+   nav: false,
+   controls: false,
 
-$(document).ready(function () {
-   $('.carousel__inner').slick({
-      speed: 1200,
-      adaptiveHeight: true,
-      prevArrow: '<button type="button" class="slick-prev" ><img src="icons/arr_left.png"></img></button>',
-      nextArrow: '<button type="button" class="slick-next" ><img src="icons/arr_right.png"></img></button>',
-      dotsClass: 'slick-dots',
-      responsive: [
-         {
-            breakpoint: 992,
-            settings: {
-               arrows: false,
-               dots: true,
-            }
-         },
-         {
-            breakpoint: 480,
-            settings: {
-               arrows: false,
-               centerMode: true,
-               centerPadding: '40px',
-               slidesToShow: 1
-            }
-         }
-      ]
-   });
+});
+document.querySelector('.next').addEventListener('click', function () {
+   slider.goTo('next');
+});
+document.querySelector('.prev').addEventListener('click', function () {
+   slider.goTo('prev');
 });
